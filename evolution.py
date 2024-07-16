59848a0f44b9755a24a2d86ab2c6c206e8740db7
Martin Hornacek <hornacek.martin@gmail.com>
2024-07-16 20:28:43 +0200

    feat: ellipses

--- evolution.py
+++ evolution.py
@@ -1,15 +1,15 @@
 import copy
 import numpy as np
+from Ellipse import Ellipse
 from helpers import calculate_rmse, clamp
-from Line import Line
 from typing import List
 
 def select_best(population, fitness, nums):
     N = len(nums)
     fitness_sorted = np.sort(fitness)
     indices_fitness_sorted = np.argsort(fitness)
-    new_population_zeros = np.zeros(N, dtype=Line)
-    new_population = np.zeros(int(np.sum(nums)), dtype=Line)
+    new_population_zeros = np.zeros(N, dtype=Ellipse)
+    new_population = np.zeros(int(np.sum(nums)), dtype=Ellipse)
     new_fitness = np.zeros((int(np.sum(nums),)))
     
     for i in range(N):
@@ -23,31 +23,37 @@ def select_best(population, fitness, nums):
     return [new_population, new_fitness]
 
 def select_sus(population, fitness, nums):
-    new_population = np.zeros(nums, dtype=Line)
+    new_population = np.zeros(nums, dtype=Ellipse)
     new_fitness = np.zeros((nums,))
     old_fitness = np.copy(fitness)
+
     population_size = len(population)
     fitness = fitness - np.min(fitness) + 1
     sum_fitness = np.sum(fitness).astype(np.float32)
 
-    w0 = np.zeros((population_size + 1,))
+    w0 = np.zeros((population_size + 1),)
 
     for i in range(population_size):
         men = fitness[i] * sum_fitness
         w0[i] = 1.0 / men # creation of inverse weights 
+
     w0[i + 1] = 0
-    w = np.zeros((population_size + 1,))
+    w = np.zeros(population_size + 1)
     for i in np.arange(population_size - 1, -1, -1):
         w[i] = w[i + 1] + w0[i]
+
     max_w = np.max(w)
     if (max_w == 0):
         max_w = 0.00001
+
     w = (w / max_w) * 100 # weigth vector
     pdel = 100.0/nums 
     b0 = np.random.uniform() * pdel - 0.00001
     b = np.zeros((nums,))
+
     for i in range(1, nums + 1):
         b[i - 1] = (i - 1) * pdel + b0
+
     for i in range(nums):
         for j in range(population_size):
             if(b[i] < w[j] and b[i] > w[j + 1]):
@@ -70,14 +76,14 @@ def evaluate_fitness(population, original_image, generated_image, fitness):
 def evaluate_partial_similarity(params):
     original_image = params[0]
     generated_image = params[1]
-    individual: Line = params[2]
-    fitness = params[3]
-    
+    individual: Ellipse = params[2]
+    fitness = params[3] 
+
     if fitness is None:
-        generated, color = individual.draw_line_on_canvas_with_color_from_image(generated_image, original_image)       
+        generated, color = individual.draw_ellipse_on_canvas_with_color_from_image(generated_image, original_image)       
         return calculate_rmse(original_image, generated)
     else:
-        generated, color = individual.draw_line_on_canvas_with_color_from_image(generated_image, original_image)  
+        generated, color = individual.draw_ellipse_on_canvas_with_color_from_image(generated_image, original_image)  
         
         if (color == 0):
             return fitness
@@ -89,65 +95,80 @@ def evaluate_partial_similarity(params):
         else:
             return fitness
         
-def generate_population(population_size, search_space, additive_mutation_space) -> List["Line"]:   
+def generate_population(population_size, search_space) -> List[Ellipse]:   
     new_population = []
 
     dX = search_space[1,0] - search_space[0,0]
-    dY = search_space[1,2] - search_space[0,2]
+    dY = search_space[1,1] - search_space[0,1]
+    dMajorAxis = search_space[1,2] - search_space[0,2]
+    dMinorAxis = search_space[1,3] - search_space[0,3]
+    dAngle = search_space[1,4] - search_space[0,4]
 
     for r in range(int(population_size)):
-        x1 = search_space[0,0] + np.random.uniform() * dX
-        x1 = clamp(x1, search_space[0,0], search_space[1,0])
+        x_center = search_space[0,0] + np.random.uniform() * dX
+        x_center = clamp(x_center, search_space[0,0], search_space[1,0])
 
-        y1 = search_space[0,2] + np.random.uniform() * dY
-        y1 = clamp(y1, search_space[0,2], search_space[1,2])
+        y_center = search_space[0,1] + np.random.uniform() * dY
+        y_center = clamp(y_center, search_space[0,1], search_space[1,1])
 
-        x2 = x1 + np.random.randint(2 * additive_mutation_space[1] + 1) - additive_mutation_space[1]
-        x2 = clamp(x2, search_space[0,1], search_space[1,1])
-          
-        y2 = y1 + np.random.randint(2 * additive_mutation_space[3] + 1) - additive_mutation_space[3] 
-        y2 = clamp(y2, search_space[0,3], search_space[1,3])
+        major_axis_length = search_space[0,2] + np.random.uniform() * dMajorAxis
+        major_axis_length = clamp(major_axis_length, search_space[0,2] + 1, search_space[1,2])
+        
+        minor_axis_length = search_space[0,3] + np.random.uniform() * dMinorAxis
+        minor_axis_length = clamp(minor_axis_length, search_space[0,3] + 1, search_space[1,3])
+        
+        rotation_angle = search_space[0,4] + np.random.uniform() * dAngle
 
-        new_population.append(Line(x1, x2, y1, y2))
+        new_population.append(Ellipse(x_center,
+                                      y_center,
+                                      major_axis_length,
+                                      minor_axis_length,
+                                      rotation_angle))
 
     return new_population
 
-def mutate(population: List[Line], factor, search_space, additive_mutation_space) -> List[Line]:
+def mutate(population: List[Ellipse], factor, search_space, additive_mutation_space) -> List[Ellipse]:
     length_population = len(population)
 
     factor = clamp(factor, 0, 1)
-    n = int(np.ceil(length_population * 4 * factor * np.random.uniform()))
+    n = int(np.ceil(length_population * 5 * factor * np.random.uniform()))
     new_population = copy.deepcopy(population)
     
     for i in range(n):
-        rN = np.random.randint(2)
-        r = int(np.ceil(np.random.uniform()*length_population)) - 1
+        rN = np.random.randint(3)
+        r = int(np.ceil(np.random.uniform() * length_population)) - 1
 
-        old_line = population[r]
-        new_line = new_population[r]
+        old_ellipse = population[r]
+        new_ellipse = new_population[r]
 
-        if rN == 0:  # x1, y1
-            mutation_x1 = (2.0 * np.random.uniform() - 1) * additive_mutation_space[0]
-            new_x1 = old_line.x1 + mutation_x1
-            new_x1 = clamp(new_x1, search_space[0, 0], search_space[1, 0])
+        if rN == 0:  # mutate center x, y
+            mutation_x_center = np.random.normal(0, additive_mutation_space[0])
+            new_x_center = old_ellipse.x_center + mutation_x_center
+            new_x_center = clamp(new_x_center, search_space[0, 0], search_space[1, 0])
 
-            mutation_y1 = (2.0 * np.random.uniform() - 1) * additive_mutation_space[2]
-            new_y1 = old_line.y1 + mutation_y1
-            new_y1 = clamp(new_y1, search_space[0, 2], search_space[1, 2])
+            mutation_y_center = np.random.normal(0, additive_mutation_space[1])
+            new_y_center = old_ellipse.y_center + mutation_y_center
+            new_y_center = clamp(new_y_center, search_space[0, 1], search_space[1, 1])
 
-            new_line.x1 = new_x1
-            new_line.y1 = new_y1
+            new_ellipse.x_center = new_x_center
+            new_ellipse.y_center = new_y_center
 
-        elif rN == 1:  # x2, y2
-            mutation_x2 = (2.0 * np.random.uniform() - 1) * additive_mutation_space[1]
-            new_x2 = old_line.x2 + mutation_x2
-            new_x2 = clamp(new_x2, search_space[0, 1], search_space[1, 1])
+        elif rN == 1:  # mutate major_axis_length, minor_axis_length
+            mutation_major = np.random.normal(0, additive_mutation_space[2])
+            new_major_axis_length = old_ellipse.major_axis_length + mutation_major
+            new_major_axis_length = clamp(new_major_axis_length, search_space[0, 2] + 1, search_space[1, 2])
 
-            mutation_y2 = (2.0 * np.random.uniform() - 1) * additive_mutation_space[3]
-            new_y2 = old_line.y2 + mutation_y2
-            new_y2 = clamp(new_y2, search_space[0, 3], search_space[1, 3])
+            mutation_minor = np.random.normal(0, additive_mutation_space[3])
+            new_minor_axis_length = old_ellipse.minor_axis_length + mutation_minor
+            new_minor_axis_length = clamp(new_minor_axis_length, search_space[0, 3] + 1, search_space[1, 3])
 
-            new_line.x2 = new_x2
-            new_line.y2 = new_y2
+            new_ellipse.major_axis_length = new_major_axis_length
+            new_ellipse.minor_axis_length = new_minor_axis_length
+
+        elif rN == 2:  # mutate angle
+            mutation_angle = np.random.normal(0, additive_mutation_space[4])
+            new_angle = (old_ellipse.rotation_angle + mutation_angle) % 360
+
+            new_ellipse.rotation_angle = new_angle
 
     return new_population
--- main.py
+++ main.py
@@ -1,19 +1,21 @@
-import cv2
 import copy
+import cv2
 import numpy as np
 import time
+from Ellipse import Ellipse
 from evolution import evaluate_fitness, generate_population, mutate, select_best, select_sus
 from helpers import array_to_image, plot_fitness_over_iterations, save_output_as_gif
-from Line import Line
 
 DETERMINISTIC_MODE = True  # reproducible results [True, False]
 DETERMINISTIC_SEED = 42 # seed for pseudo-random generator
 GENERATE_GIF = True # generate animation [True, False]
+MAJOR_AXIS_MAX_LENGTH = 15
+MINOR_AXIS_MAX_LENGTH = 10
 MAX_ADDMUT = 5 # [%] maximum aditive mutation range
 MUT_RATE = 20 # [%] mutation rate (percentage of individuals to be mutated)
 NEvo = 10 # number of evolution steps per one object 
 NUMBER_OF_OBJECTS = 6000
-SEARCH_SPACE_SIZE = 4
+SEARCH_SPACE_SIZE = 5
 
 def print_best_fitness(population, fitness, start_time):    
     best, best_fitness = select_best(population, fitness, [1]) # find out the final solution
@@ -34,15 +36,15 @@ generated_image = 255 * np.ones((original_image_height, original_image_width), d
 
 search_space = np.concatenate((np.zeros((1, SEARCH_SPACE_SIZE)), # lower bound
                                np.array([[original_image_width - 1,
-                                          original_image_width - 1,
                                           original_image_height - 1,
-                                          original_image_height - 1
-                                          ]])
+                                          MAJOR_AXIS_MAX_LENGTH - 1,
+                                          MINOR_AXIS_MAX_LENGTH - 1,
+                                          360 - 1]])
                                ), axis = 0) # upper bound
 
 additive_mutation_space = search_space[1,:] * (MAX_ADDMUT / 100.0) # range of changes for the additive mutation
 
-data = np.zeros((NUMBER_OF_OBJECTS, 6)) # (x1,x2,y1,y2,color,fitness)
+data = np.zeros((NUMBER_OF_OBJECTS, 7)) # (center_x,center_y,major_axis_length,minor_axis_length,angle,color,fitness)
 fitness_over_iterations = []
 current_fitness = None # initial fitness value
 buffer = 0 # auxiliary variable to stop evolution if no changes occur
@@ -55,18 +57,18 @@ if GENERATE_GIF:
 start_time = time.time() # start the timer
 
 while count <= NUMBER_OF_OBJECTS:
-    next_population = generate_population(24, search_space, additive_mutation_space)
+    next_population = generate_population(24, search_space)
     next_population_fitness = evaluate_fitness(next_population, original_image, generated_image, current_fitness)
 
     for i in range(NEvo):
         previous_population = copy.deepcopy(next_population)
         previous_population_fitness = np.copy(next_population_fitness)
 
-        next_population_best, next_population_fitness_best = select_best(previous_population, next_population_fitness, [3,2,1])
-        next_population_sus, next_population_fitness_sus = select_sus(previous_population, next_population_fitness, 18)
+        next_population_best, next_population_fitness_best = select_best(previous_population, previous_population_fitness, [3,2,1])
+        next_population_sus, next_population_fitness_sus = select_sus(previous_population, previous_population_fitness, 18)
 
         next_population_sus = mutate(next_population_sus, MUT_RATE / 100.0, search_space, additive_mutation_space)        
-        next_population = np.concatenate((next_population_best, next_population_sus), axis=0)
+        next_population = np.concatenate((next_population_best, next_population_sus), axis = 0)
         next_population_fitness = evaluate_fitness(next_population, original_image, generated_image, current_fitness)
         
         if (np.min(next_population_fitness) == np.min(previous_population_fitness)):
@@ -80,28 +82,30 @@ while count <= NUMBER_OF_OBJECTS:
     
     if current_fitness is None:
         current_fitness = 1e6
-  
+    
     if best_fitness[0] < current_fitness:
         current_fitness = best_fitness[0]
         fitness_over_iterations.append(current_fitness)
 
-        best_individual: Line = best[0]
-        generated_image, color = best_individual.draw_line_on_canvas_with_color_from_image(generated_image, original_image)
+        best_individual: Ellipse = best[0]
+        generated_image, color = best_individual.draw_ellipse_on_canvas_with_color_from_image(generated_image, original_image)
 
         print("# " + str(count) + " Fitness: " + str(current_fitness))
         data[count - 1, :] = np.concatenate(np.array(
-                (int(best_individual.x1),
-                 int(best_individual.x2),
-                 int(best_individual.y1),
-                 int(best_individual.y2),
-                color,
-                current_fitness)
-            ).reshape(6,1))
+                (int(best_individual.x_center),
+                 int(best_individual.y_center),
+                 int(best_individual.major_axis_length),
+                 int(best_individual.minor_axis_length),
+                 int(best_individual.rotation_angle),
+                 color,
+                 current_fitness)
+            ).reshape(7,1))
         
         count += 1
 
         if GENERATE_GIF:
             images.append(array_to_image(generated_image))
+
 
 plot_fitness_over_iterations(fitness_over_iterations)
 print_best_fitness(next_population, next_population_fitness, start_time)
